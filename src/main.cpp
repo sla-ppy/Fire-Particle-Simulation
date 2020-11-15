d8f5670fdf3e452038aa085359c46394d883b789
S4kyt <jerikolo1@gmail.com>
2020-11-15 12:12:38 +0100

    Animating Colors

--- src/main.cpp
+++ src/main.cpp
@@ -24,14 +24,19 @@ int main(int argc, char* argv[]) {
 	while (true) {
 		// Update particles
 
+		// Number of ms since program started
+		int elapsed = SDL_GetTicks();
+
+		unsigned char green = (unsigned char)((1 + sin(elapsed * 0.0001)) * 128);
+		unsigned char red = (unsigned char)((1 + sin(elapsed * 0.0002)) * 128);
+		unsigned char blue = (unsigned char)((1 + sin(elapsed * 0.0003)) * 128);
+
 		// Draw particles
 		for (int y = 0; y < Screen::SCREEN_HEIGHT; y++) {
 			for (int x = 0; x < Screen::SCREEN_WIDTH; x++) {
-				screen.setPixel(x, y, 128, 0, 255);
+				screen.setPixel(x, y, red, green, blue);
 			}
 		}
-
-		screen.setPixel(400, 300, 255, 255, 255);
 
 		// Draw the screen
 		screen.update();
