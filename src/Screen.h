617cff48ba216bd7e42295eb5ea7a750f3ceaba2
S4kyt <jerikolo1@gmail.com>
2020-11-15 11:42:54 +0100

    Adding a Set pixel method

--- src/Screen.h
+++ src/Screen.h
@@ -1,28 +1,37 @@
+/*
+ * Screen.h
+ *
+ *  Created on: 1 Sep 2014
+ *      Author: johnwpurcell
+ */
+
 #ifndef SCREEN_H_
 #define SCREEN_H_
 
 #include <SDL.h>
 
-namespace n_Screen {
+namespace caveofprogramming {
 
-class Screen {
-public:
-	const static int SCREEN_WIDTH = 800;
-	const static int SCREEN_HEIGHT = 600;
+	class Screen {
+	public:
+		const static int SCREEN_WIDTH = 800;
+		const static int SCREEN_HEIGHT = 600;
 
-private:
-	SDL_Window *m_window;
-	SDL_Renderer *m_renderer;
-	SDL_Texture *m_texture;
-	Uint32 *m_buffer;
+	private:
+		SDL_Window* m_window;
+		SDL_Renderer* m_renderer;
+		SDL_Texture* m_texture;
+		Uint32* m_buffer;
 
-public:
-	Screen();
-	bool init();
-	bool processEvents();
-	void close();
-};
+	public:
+		Screen();
+		bool init();
+		void update();
+		void setPixel(int x, int y, Uint8 red, Uint8 green, Uint8 blue);
+		bool processEvents();
+		void close();
+	};
 
-} /* namespace n_Screen */
+} /* namespace caveofprogramming */
 
 #endif /* SCREEN_H_ */
